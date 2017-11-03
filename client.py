e88ba16459bd4d626b16534968f551b6816e3edd
Danny Byrd <byrd106@mail.chapman.edu>
2017-11-02 19:02:19 -0600

    Point client to the correct place

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ CLIENT_TEAM = "X"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 50007)
+server_address = ('localhost', 707)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
